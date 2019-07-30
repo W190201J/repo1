471c3658fe273f0bfdbafb4607f6ab5f43d471cd
bilibilibenson <48378899+bilibilibenson@users.noreply.github.com>
2019-07-30 13:00:35 +0800

    project

---
+++ superDeliver/src/main/java/com/newer/deliver/SuperDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.newer.deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDeliverApplication.class, args);
+	}
+
+}
